8c1914db025765bb1bcd3e30d0bdb8da07b57178
Assami Muzaki <muzakiassami@gmail.com>
2022-09-26 20:50:29 +0700

    init ckeditor online builder

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,38 @@
-import './App.css';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { useState } from 'react';
+import React, { Component } from 'react';
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
+import { CKEditor } from '@ckeditor/ckeditor5-react'
 
-function App() {
+const editorConfiguration = {
+    toolbar: [ 'bold', 'italic' ]
+};
 
-  const [textResult, setTextResult] = useState('dummy')
-
-  return (
-    <div className='main'>
-      <h1>CKeditor Form</h1>
-      <div className='ckeditor-form'>
-        <CKEditor
-            editor={ ClassicEditor }
-            data="<p>Hello from CKEditor 5!</p>"
-            // onReady={ editor => {
-            //     // You can store the "editor" and use when it is needed.
-            //     console.log( 'Editor is ready to use!', editor );
-            // } }
-            onChange={ ( event, editor ) => {
-                const data = editor.getData();
-                setTextResult(data)
-            } }
-            // onBlur={ ( event, editor ) => {
-            //     console.log( 'Blur.', editor );
-            // } }
-            // onFocus={ ( event, editor ) => {
-            //     console.log( 'Focus.', editor );
-            // } }
-        />
-      </div>
-      <div className="result">
-        <h3>HTML Result</h3>
-        <p>{textResult}</p>
-      </div>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>Using CKEditor 5 from online builder in React</h2>
+                <CKEditor
+                    editor={ Editor }
+                    config={ editorConfiguration }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
