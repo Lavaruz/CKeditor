6969b6b4d210363eef0106190e2e18a301677b99
Assami Muzaki <muzakiassami@gmail.com>
2022-09-27 19:53:17 +0700

    back to basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,35 @@
-import React, { Component } from 'react';
-import Editor from 'ckeditor5-custom-build/build/ckeditor';
-import { CKEditor } from '@ckeditor/ckeditor5-react'
+import React, { useState } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 import './App.css'
 
-const editorConfiguration = {
-    toolbar: [ 'bold', 'italic' ]
-};
+function App(){
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-              <div className="main">
-                <h2>Using CKEditor 5 from online builder in React</h2>
-                <CKEditor
-                    editor={ Editor }
-                    config={ editorConfiguration }
-                    data="<p>Hello from CKEditor 5!</p>"
+    const [result, setResult] = useState('Result Here')
+
+    return(
+        <div className="main">
+            <h2>Input Raw Text</h2>
+            <div className="main-ckeditor">
+            <CKEditor
+                    editor={ ClassicEditor }
+                    data=""
                     onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
                         console.log( 'Editor is ready to use!', editor );
                     } }
                     onChange={ ( event, editor ) => {
                         const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
+                        setResult(data)
                     } }
                 />
-              </div>
             </div>
-        );
-    }
+            <h2>Coverted Text to HTML Tag</h2>
+            <div className="main-result">
+                {result}
+            </div>
+        </div>
+    )
 }
 
 export default App;
